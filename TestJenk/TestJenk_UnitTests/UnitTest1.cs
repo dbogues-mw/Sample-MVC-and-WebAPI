021eb22b3dd746e55dfa3dea396c4885cfa4c7be
dbogues-mw <davidb@merchantwarehouse.com>
2013-12-11 16:09:33 +0000

    Unit  Tests

---
+++ TestJenk/TestJenk_UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestJenk_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            int x = 2 + 2;
+            Assert.AreEqual(4, x);
+
+        }
+    }
+}
