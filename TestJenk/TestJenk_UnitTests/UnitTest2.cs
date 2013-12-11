2344447d38e6c6686f9dba16fc435eb9587b1885
dbogues-mw <davidb@merchantwarehouse.com>
2013-12-11 16:48:19 +0000

    fail test

---
+++ TestJenk/TestJenk_UnitTests/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestJenk_UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
