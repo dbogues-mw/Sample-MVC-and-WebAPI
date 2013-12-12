e7fbf66bb5d82e07c7962748900ec41315ddc530
dbogues-mw <davidb@merchantwarehouse.com>
2013-12-12 11:53:21 +0000

    Test Library

--- TestJenk/TestJenk/Controllers/HomeController.cs
+++ TestJenk/TestJenk/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestLIbrary;
 
 namespace TestJenk.Controllers
 {
@@ -12,6 +13,10 @@ namespace TestJenk.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
+
+            TestLibrary tl = new TestLibrary();
+            tl.TestLibraryMethod();
+
             return View();
         }
 	}
---
+++ TestJenk/TestLIbrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLIbrary
+{
+    public class TestLibrary
+    {
+        public int TestLibraryMethod()
+        {
+            return 62;
+        }
+    }
+}
